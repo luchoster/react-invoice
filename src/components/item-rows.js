66b8ca16f1e0c15fd357b67ceaf134e76b3db844
Lucho Suarez <luis.suarez@rrpartners.com>
2017-11-02 20:57:03 -0700

    small changes to the table rows, to make it editable

--- src/components/item-rows.js
+++ src/components/item-rows.js
@@ -3,33 +3,66 @@ import ReactTable from 'react-table'
 
 class ItemsRows extends React.Component {
   state = {
-    data: []
+    pageSize: 2,
+    data: [{}]
   }
+
+  renderEditable = (cellInfo) =>
+    <div
+      style={{ backgroundColor: "#fafafa" }}
+      contentEditable
+      suppressContentEditableWarning
+      onBlur={e => {
+        const data = [...this.state.data];
+        data[cellInfo.index][cellInfo.column.id] = e.target.innerHTML;
+        this.setState({ data });
+      }}
+      dangerouslySetInnerHTML={{
+        __html: this.state.data[cellInfo.index][cellInfo.column.id]
+      }}
+    />
+
   render() {
     return(
-      <div>
+      <div className="col">
         <ReactTable
           data={this.state.data}
+          noDataText="Oh Noes!"
+          pageSize={this.state.pageSize}
           columns={[
             {
               Header: "Item",
-              accessor: "item"
+              accessor: "item",
+              className: "col",
+              Cell: this.renderEditable,
+              id: "item"
             },
             {
               Header: "Description",
-              accessor: "description"
+              accessor: "description",
+              className: "col",
+              Cell: this.renderEditable,
             },
             {
               Header: "Rate",
-              accessor: "price"
+              accessor: "rate",
+              className: "col",
+              Cell: this.renderEditable,
+              id: "rate"
             },
             {
               Header: "Qty",
-              accessor: "qty"
+              accessor: "qty",
+              className: "col",
+              Cell: this.renderEditable,
+              id: "qty"
             },
             {
               Header: "Price",
-              accessor: "price"
+              accessor: "price",
+              className: "col",
+              Cell: this.renderEditable,
+              id: "price"
             }
           ]}
         />
